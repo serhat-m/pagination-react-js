5a663a36759a1e5abb3322c06c58d024a792b1b8
Serhat Mutlu <mtlserhat@outlook.com>
2024-12-10 22:57:56 +0100

    refactor: ui and package structure

--- src/source.ts
+++
@@ -1,4 +0,0 @@
-import { usePagination, type TFnOptions, type TPaginationData } from "./hooks/usePagination"
-import generateTestData from "./functions/generateTestData"
-
-export { usePagination, generateTestData, type TFnOptions, type TPaginationData }
